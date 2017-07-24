69988025f47fa126257ecd9da123492f78c74a53
JOAQUIN-PC <JOAQUIN-PC@DESKTOP-9PURGGT>
2017-07-24 21:07:10 +0200

    - Creación del controlador de Sedes y Delegaciones. - Nuevas vistas de Sedes: Listado, Nueva, Editar, Detalles y Eliminar. - Cambio en rutas del menú de _Layout.cshtml - Modificación de la variable en la definición de la tabla para mostrar 10 resultados por página por defecto. - Generado diagrama de clases provisional.

---
+++ GestorONG/Controllers/SedesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GestorONG.DAL;
+using GestorONG.DataModel;
+
+namespace GestorONG.Controllers
+{
+    public class SedesController : Controller
+    {
+        private GestorONGDContext db = new GestorONGDContext();
+
+        // GET: Sedes
+        public ActionResult Index()
+        {
+            return View(db.sedes_delegaciones.ToList());
+        }
+
+        // GET: Sedes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            sede_delegacion sede_delegacion = db.sedes_delegaciones.Find(id);
+            if (sede_delegacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sede_delegacion);
+        }
+
+        // GET: Sedes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sedes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,direccion,codigoPostal,localidad,provincia,pais,personaContacto,emailContacto,telefonoContacto")] sede_delegacion sede_delegacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.sedes_delegaciones.Add(sede_delegacion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sede_delegacion);
+        }
+
+        // GET: Sedes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            sede_delegacion sede_delegacion = db.sedes_delegaciones.Find(id);
+            if (sede_delegacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sede_delegacion);
+        }
+
+        // POST: Sedes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,direccion,codigoPostal,localidad,provincia,pais,personaContacto,emailContacto,telefonoContacto")] sede_delegacion sede_delegacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sede_delegacion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sede_delegacion);
+        }
+
+        // GET: Sedes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            sede_delegacion sede_delegacion = db.sedes_delegaciones.Find(id);
+            if (sede_delegacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sede_delegacion);
+        }
+
+        // POST: Sedes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            sede_delegacion sede_delegacion = db.sedes_delegaciones.Find(id);
+            db.sedes_delegaciones.Remove(sede_delegacion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
