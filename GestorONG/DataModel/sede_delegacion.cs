3e96f1ee650e0782ea68e2fb6bbac6a9aa556d04
JOAQUIN-PC <JOAQUIN-PC@DESKTOP-9PURGGT>
2017-07-24 21:30:03 +0200

    - Modificación del nombre del atributo en las tablas y etiquetas en el modelo sedes_delegacion.cs - Formato en dos columnos de la vista de añadir una nueva sede.

--- GestorONG/DataModel/sede_delegacion.cs
+++ GestorONG/DataModel/sede_delegacion.cs
@@ -2,6 +2,7 @@ namespace GestorONG.DataModel
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     
     /// <summary>
     /// Clase para almacenar los datos de una sede/delegación
@@ -58,46 +59,55 @@ namespace GestorONG.DataModel
         /// <summary>
         /// Nombre de la delegación
         /// </summary>
+        [DisplayName("Nombre")]
         public string nombre { get; set; }
 
         /// <summary>
         /// Dirección postal de la delegación
         /// </summary>
+        [DisplayName("Direccion")]
         public string direccion { get; set; }
 
         /// <summary>
         /// Código postal de la delegación
         /// </summary>
+        [DisplayName("CP")]
         public string codigoPostal { get; set; }
 
         /// <summary>
         /// Localidad dónde está ubicada la sede o delegación
         /// </summary>
+        [DisplayName("Localidad")]
         public string localidad { get; set; }
 
         /// <summary>
         /// Provincia de la localidad de la sede
         /// </summary>
+        [DisplayName("Provincia")]
         public string provincia { get; set; }
 
         /// <summary>
         /// País en el que se encuentra la sede o delegación
         /// </summary>
+        [DisplayName("País")]
         public string pais { get; set; }
 
         /// <summary>
         /// Persona de contacto por la que preguntar de la sede o delegación
         /// </summary>
+        [DisplayName("Persona de Contacto")]
         public string personaContacto { get; set; }
 
         /// <summary>
         /// Email de contacto de la sede o delegación para contactar con la persona de contacto
         /// </summary>
+        [DisplayName("Email de Contacto")]
         public string emailContacto { get; set; }
 
         /// <summary>
         /// Teléfono de contacto de la sede o delegación para contactar con la persona de contacto
         /// </summary>
+        [DisplayName("Teléfono de Contacto")]
         public string telefonoContacto { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
